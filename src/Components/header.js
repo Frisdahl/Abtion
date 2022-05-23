cdf85f9b49dbfc3d052c9200cb8b2f4591935998
Frisdahl <74907581+Frisdahl@users.noreply.github.com>
2022-05-23 11:28:35 +0200

    simpel start på header

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import '../sass/main.scss'
+import Logo from '../assets/images/logo-sort.png'
 
 // Styles
 
 export default function Header() {
     return (
-        <div class="parent">
-            <div class="child">hello</div>
-        </div>
+        <header>
+            <div class="logo-container">
+                <img src={Logo} height={25} width={35} alt="" />
+            </div>
+
+            <nav>
+                <ul>
+                    <li>Cases</li>
+                    <li>Om os</li>
+                    <li>Viden</li>
+                    <li>Karriere</li>
+                    <li>Kontakt</li>
+                </ul>
+            </nav>
+        </header>
     )
 }
